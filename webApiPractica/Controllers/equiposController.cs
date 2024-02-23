0b70e69548bcac7ff7820ab538f0b8e92a66175a
Fracisco Chacon <Fracisco Chacon@DESKTOP-6G7DPU6>
2024-02-23 09:41:58 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+        public equiposController(equiposContext equiposContext)
+        {
+            _equiposContext = equiposContext;
+
+        }
+
+        /// <sumary>
+        /// EndPoint que retorna el listado de todos los equipos existentes
+        /// </sumary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContext.equipos
+                                            select e).ToList();
+
+            if (listadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+    }
+}
