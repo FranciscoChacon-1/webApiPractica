05acff170f890bea522439c18559be7134e1639d
Fracisco Chacon <Fracisco Chacon@DESKTOP-6G7DPU6>
2024-03-07 19:35:22 -0600

    Le agregue lo que faltaba y arregle errores

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -30,27 +30,30 @@ namespace webApiPractica.Controllers
         public IActionResult Get()
         {
             var listadoEquipo = (from e in _equiposContext.equipos
-                                           join t in _equiposContext.tipo_equipo
-                                           on e.tipo_equipo_id equals t.id_tipo_equipo
-                                           join m in _equiposContext.marcas
-                                           on e.marca_id equals m.id_marcas
-                                           join es in _equiposContext.estados_equipo
-                                           on e.estado_equipo_id equals es.id_estados_equipo
-                                           select new 
-                                           {
-                                               e.id_equipos,
-                                               e.nombre,
-                                               e.descripcion,
-                                               e.tipo_equipo_id,
-                                               tipo_equipo = t.descripcion,
-                                               e.marca_id,
-                                               marca = m.nombre_marca,
-                                               e.estado_equipo_id,
-                                               estado_equipo = es.descripcion,
-                                               detalle = $"tipo : {t.descripcion}, Marca {m.nombre_marca}, Estado eqipo {es.descripcion}",
-                                               e.estado
-                                           }
-                                           ).ToList();
+                                 join t in _equiposContext.tipo_equipo
+                                 on e.tipo_equipo_id equals t.id_tipo_equipo
+                                 join m in _equiposContext.marcas
+                                 on e.marca_id equals m.id_marcas
+                                 join es in _equiposContext.estados_equipo
+                                 on e.estado_equipo_id equals es.id_estados_equipo
+                                 select new
+                                 {
+                                     e.id_equipos,
+                                     e.nombre,
+                                     e.descripcion,
+                                     e.tipo_equipo_id,
+                                     tipo_equipo = t.descripcion,
+                                     e.marca_id,
+                                     marca = m.nombre_marca,
+                                     e.estado_equipo_id,
+                                     estado_equipo = es.descripcion,
+                                     detalle = $"Tipo : {t.descripcion}, Marca {m.nombre_marca}, Estado equipo {es.descripcion}",
+                                     e.estado
+                                 }
+                                           ).OrderBy(resultado=>resultado.estado_equipo_id)
+                                           .ThenBy(resultado => resultado.marca_id)
+                                           .ThenByDescending(resultado => resultado.tipo_equipo_id)
+                                           .ToList();
 
 
             if (listadoEquipo.Count() == 0)
