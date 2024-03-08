e902bb6e92605fa03a2a27859cd2c771467d675e
ASUS VivoBook <ASUS VivoBook@DESKTOP-LIARBAQ>
2024-03-07 19:16:15 -0600

    un avznaze en el gett all

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -30,7 +30,28 @@ namespace webApiPractica.Controllers
         public IActionResult Get()
         {
             List<equipos> listadoEquipo = (from e in _equiposContext.equipos
-                                           select e).ToList();
+                                           join t in _equiposContext.tipo_equipo
+                                           on e.tipo_equipo_id equals t.id_tipo_equipo
+                                           join m in _equiposContext.marcas
+                                           on e.marca_id equals m.id_marcas
+                                           join es in _equiposContext.estados_equipo
+                                           on e.estado_equipo_id equals es.id_estados_equipo
+                                           select new 
+                                           {
+                                               e.id_eqipos,
+                                               e.nombre,
+                                               e.descripcion,
+                                               e.tipo_equipo_id,
+                                               tipo_equipo = t.descripcion,
+                                               e.marca_id,
+                                               marca = m.nombre_marca,
+                                               e.estado_equipo_id,
+                                               estado_equipo = es.descripcion,
+                                               detalle = $"tipo : {t.descripcion}, Marca {m.nombre_marca}, Estado eqipo {es.descripcion}",
+                                               e.estado
+                                           }
+                                           ).ToList();
+
 
             if (listadoEquipo.Count() == 0)
             {
